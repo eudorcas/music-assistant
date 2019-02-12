12c0052f3eea6db237a60b5340a5cb3729e46abd
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-02-12 17:05:57 +0100

    PianoKey Component

---
+++ src/components/PianoKey/PianoKey.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './PianoKey.css';
+import Sound from "react-sound";
+
+const pianoKey = (props) => {
+    const color = props.type === "white" ? classes.white : classes.black;
+    const soundPath = require("../../assets/audio/" + props.value + ".mp3");
+    return (
+        <div className={color} onClick={props.clicked}>
+            <Sound url={soundPath}
+                   playStatus={props.playing === props.value ? Sound.status.PLAYING : Sound.status.STOPPED}/>
+            <Sound url={soundPath}
+                   playStatus={props.playing === props.value.toUpperCase() ? Sound.status.PLAYING : Sound.status.STOPPED}/>
+        </div>
+    )
+};
+
+export default pianoKey;
--- src/containers/Piano/Piano.js
+++ src/containers/Piano/Piano.js
@@ -1,88 +1,82 @@
-import React, { Component} from 'react';
-import Sound from 'react-sound';
-import fast from '../../assets/audio/cSharp4.mp3';
-import slow from '../../assets/audio/d4.mp3';
+import React, {Component} from 'react';
+import PianoKey from '../../components/PianoKey/PianoKey';
+
 import classes from './Piano.css';
+// import Sound from "react-sound";
+// import d4 from "../../assets/audio/d4.mp3";
+// import csharp4 from '../../assets/audio/csharp4.mp3';
 
 const pianoKeys = [
-     {value: "c3", type: "white"},
-    {value: "c#3", type: "black"},
-    {value: "d", type: "white"},
-    {value: "d#3", type: "black"},
-    {value: "e3", type: "white"},
-    {value: "f3", type: "white"},
-    {value: "f#3", type: "black"}
-
+    // {value: "c4", type: "white"},
+    {value: "csharp4", type: "black"},
+    {value: "d4", type: "white"}
+    // {value: "dsharp4", type: "black"},
+    // {value: "e4", type: "white"},
+    // {value: "f4", type: "white"},
+    // {value: "fsharp4", type: "black"},
+    // {value: "g4", type: "white"},
+    // {value: "gsharp4", type: "black"},
+    // {value: "a4", type: "white"},
+    // {value: "asharp4", type: "black"},
+    // {value: "b4", type: "white"},
+    // {value: "c5", type: "white"},
+    // {value: "csharp5", type: "black"},
+    // {value: "d5", type: "white"},
+    // {value: "dsharp5", type: "black"},
+    // {value: "e5", type: "white"},
+    // {value: "f5", type: "white"},
+    // {value: "fsharp5", type: "black"},
+    // {value: "g5", type: "white"},
+    // {value: "gsharp5", type: "black"},
+    // {value: "a5", type: "white"},
+    // {value: "asharp5", type: "black"},
+    // {value: "b5", type: "white"},
+    // {value: "c6", type: "white"}
 ];
 
+
+
 class Piano extends Component {
-    state= {
-        playing: "",
-        repeat: false
+
+
+    state = {
+        playing: ""
     };
-    blackHandleClick = () => {
-        console.log("black");
-        if (this.state.playing==="fast") {
-            this.setState({repeat: true})
-        }
-        else {
+
+
+    handlePlay = (value) => {
+        if (this.state.playing === value) {
             this.setState({
-                playing: "fast",
-                repeat: false
-            })
-        }
-    };
-    whiteHandleClick = () => {
-        console.log("white");
-        if (this.state.playing==="slow") {
-            this.setState({repeat: true})
-        }
-        else {
+                playing: value.toUpperCase()
+            });
+        } else {
             this.setState({
-                playing: "slow",
-                repeat: false
-            })
+                playing: value
+            });
         }
 
     };
+
 
     render() {
-        console.log("render");
-        const whiteDesktop = [classes.white, classes.desktop];
-        const blackDesktop = [classes.black, classes.desktop];
+        console.log("render", this.state.playing);
+        const piano = pianoKeys.map(el => {
+            return <PianoKey playing={this.state.playing} key={el.value} type={el.type} value={el.value}
+                             clicked={() => this.handlePlay(el.value)}>
+            </PianoKey>
+        });
+        // const whiteDesktop = [classes.white, classes.desktop];
+        // const blackDesktop = [classes.black, classes.desktop];
         return (
-        <div className={classes.Piano}>
-            <p>{this.state.repeat ? "true" : "false"}</p>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.black} onClick={this.blackHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.black} onClick={this.blackHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.black} onClick={this.blackHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.black} onClick={this.blackHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.black} onClick={this.blackHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={classes.white} onClick={this.whiteHandleClick}></div>
-            <div className={blackDesktop.join(" ")} onClick={this.blackHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={blackDesktop.join(" ")} onClick={this.blackHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={blackDesktop.join(" ")} onClick={this.blackHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={blackDesktop.join(" ")} onClick={this.blackHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={blackDesktop.join(" ")} onClick={this.blackHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <div className={whiteDesktop.join(" ")} onClick={this.whiteHandleClick}></div>
-            <Sound url={fast} playStatus={((this.state.playing==='fast' && this.state.repeat) || this.state.playing==='fast') ? Sound.status.PLAYING : Sound.status.STOPPED} />
-            <Sound url={slow} playStatus={((this.state.playing==='slow' &&  this.state.repeat) || this.state.playing==='slow')  ? Sound.status.PLAYING : Sound.status.STOPPED}/>
-        </div>
+            <div className={classes.Piano}>
+                {piano}
+            </div>
         )
     }
 }
 
 export default Piano;
+
+
+// {/*<Sound url={slow}*/}
+// {/*playStatus={((this.state.playing === 'slow' && this.state.repeat) || this.state.playing === 'slow') ? Sound.status.PLAYING : Sound.status.STOPPED}/>*/}
